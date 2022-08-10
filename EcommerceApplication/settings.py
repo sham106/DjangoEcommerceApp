c12e6cd33379808eff4661a45fedd6fc840a0113
Shamabach Simiyu <shamabachs.s@pop-os.localdomain>
2022-08-10 15:54:34 +0200

    settings

--- EcommerceApplication/settings.py
+++ EcommerceApplication/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3' ),
     }
 }
 
