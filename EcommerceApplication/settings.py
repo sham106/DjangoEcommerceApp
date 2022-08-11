6eb5aca53c68e004a5c39fdf808924ffef786e31
Shamabach Simiyu <shamabachs.s@pop-os.localdomain>
2022-08-11 11:31:33 +0200

    database changes

--- EcommerceApplication/settings.py
+++ EcommerceApplication/settings.py
@@ -85,9 +85,10 @@ WSGI_APPLICATION = 'EcommerceApplication.wsgi.application'
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3' ),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': str(BASE_DIR / 'db.sqlite3' ),
     }
 }
 
